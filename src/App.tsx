7c2dc67486fd4b508e26004098124a1e14fdf910
Alex Lowe <loweman64@outlook.com>
2022-07-08 14:56:01 -0400

    feat: Add Loading Icon When Getting Data from API

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,40 @@ import ListContainer from "./components/ListContainer";
 import ListBody from "./components/ListBody";
 import ListRow from "./components/ListRow";
 import {getStudents} from "./services/student";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons";
 
 
 function App() {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getStudents().then(data => {
             setData(data);
+            setIsLoading(false);
         }).catch(err => {
+            setIsLoading(false);
             console.log(err);
         })
     }, []);
 
     return (
         <div className="App">
-            <Header />
+            <Header/>
             <div className="Body-Container">
                 <ListContainer>
-                    <ListHeader />
+                    <ListHeader/>
                     <ListBody>
-                        {
-                            data.map((item, index) => {
-                                return (
-                                    ListRow({data: item})
-                                )
-                            })
-                        }
+                        <>
+                            {isLoading ? <FontAwesomeIcon icon={faSpinner} className="fa-spin-pulse Loading-Icon"/> :
+                                data.map(item => {
+                                    return (
+                                        ListRow({data: item})
+                                    )
+                                })
+                            }
+                        </>
                     </ListBody>
                 </ListContainer>
             </div>
