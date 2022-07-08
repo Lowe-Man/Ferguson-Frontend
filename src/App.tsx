295dc05e04f7a5d1ed37586f8b271f2527a12200
Alex Lowe <loweman64@outlook.com>
2022-07-08 14:30:29 -0400

    feat: Get API Data and Create Grid Columns and Rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import Header from "./components/Header";
 import ListHeader from "./components/ListHeader";
 import ListContainer from "./components/ListContainer";
 import ListBody from "./components/ListBody";
 import ListRow from "./components/ListRow";
+import {getStudents} from "./services/student";
 
 
 function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getStudents().then(data => {
+            setData(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    }, []);
+
     return (
         <div className="App">
             <Header />
@@ -15,8 +26,13 @@ function App() {
                 <ListContainer>
                     <ListHeader />
                     <ListBody>
-                        <ListRow />
-                        <ListRow />
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    ListRow({data: item})
+                                )
+                            })
+                        }
                     </ListBody>
                 </ListContainer>
             </div>
--- src/components/ListRow.tsx
+++ src/components/ListRow.tsx
@@ -1,15 +1,15 @@
 import './ListRow.scss';
 import ListItem from "./ListItem";
 
-function ListRow() {
+function ListRow(props: any) {
     return (
         <div className="List-Row">
             <div className="Item-Container">
-                <ListItem item="First Name"/>
-                <ListItem item="Last Name"/>
-                <ListItem item="Phone Number"/>
-                <ListItem item="State Code"/>
-                <ListItem item="State Name"/>
+                <ListItem item={props.data.FirstName}/>
+                <ListItem item={props.data.LastName}/>
+                <ListItem item={props.data.PhoneNumber}/>
+                <ListItem item={props.data.StateCode}/>
+                <ListItem item={props.data.StateName}/>
             </div>
             <button type="button">View</button>
         </div>
