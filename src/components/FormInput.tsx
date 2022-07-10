893e6243c566bb0552b5d5118810a48ff01f7aad
Alex Lowe <loweman64@outlook.com>
2022-07-10 16:26:04 -0400

    fix: Form Input readOnly State

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -3,8 +3,15 @@ import './FormInput.scss';
 function FormInput(props: any) {
     return (
         <div className="Form-Input" onBlur={props.onBlur} onFocus={props.onFocus}>
-            {props.readOnly ? <label htmlFor={props.id} className="input-active">{props.label}</label> : <input id={props.id} type={props.type}/>}
-            {props.readOnly ? <input type={props.type} id={props.id} name={props.id} value={props.value} readOnly /> : <input type={props.type} id={props.id} name={props.id} value={props.value}/>}
+            {
+                props.readOnly ? <>
+                    <label htmlFor={props.id} className="input-active">{props.label}</label>
+                    <input type={props.type} id={props.id} name={props.id} value={props.value} readOnly />
+                </> : <>
+                    <label htmlFor={props.id}>{props.label}</label>
+                    <input type={props.type} id={props.id} name={props.id} readOnly />
+                </>
+            }
         </div>
     );
 }
