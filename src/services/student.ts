ad31b7b5b19bf8f05a28c197e5a10c5d9458e4fc
Alex Lowe <loweman64@outlook.com>
2022-07-10 20:04:26 -0400

    refactor: Remove Code Duplication when Returning Student(s)

--- src/services/student.ts
+++ src/services/student.ts
@@ -1,4 +1,22 @@
+import Student from "../classes/Student";
+
 const API_URL = "http://108.235.200.16:10003";
+
+function studentResponse(student: any) {
+    const phonePartOne = student.PhoneNumber.toString().substring(0, 3);
+    const phonePartTwo = student.PhoneNumber.toString().substring(3, 6);
+    const phonePartThree = student.PhoneNumber.toString().substring(6, 10);
+    const phoneString = `(${phonePartOne}) ${phonePartTwo}-${phonePartThree}`;
+    return {
+        _id: student._id,
+        FirstName: student.FirstName,
+        LastName: student.LastName,
+        PhoneNumber: phoneString,
+        StateCode: student.StateCode.StateCode,
+        StateName: student.StateCode.StateName
+    }
+
+}
 
 export async function getStudents(): Promise<any> {
     const myHeaders = new Headers();
@@ -9,15 +27,8 @@ export async function getStudents(): Promise<any> {
         redirect: 'follow'
     });
     const result = await response.json();
-    return result.map((student: { _id: string; FirstName: string; LastName: string; PhoneNumber: Number; StateCode: {StateCode: string; StateName: string }}) => {
-        return {
-            _id: student._id,
-            FirstName: student.FirstName,
-            LastName: student.LastName,
-            PhoneNumber: student.PhoneNumber,
-            StateCode: student.StateCode.StateCode,
-            StateName: student.StateCode.StateName
-        }
+    return result.map((student: { _id: string; FirstName: string; LastName: string; PhoneNumber: Number; StateCode: { StateCode: string; StateName: string } }) => {
+        return studentResponse(student);
     });
 }
 
@@ -30,20 +41,13 @@ export async function getStudent(id: string): Promise<any> {
         redirect: 'follow'
     });
     const result = await response.json();
-    return result.map((student: { _id: string; FirstName: string; LastName: string; PhoneNumber: Number; StateCode: {StateCode: string; StateName: string }}) => {
-        return {
-            _id: student._id,
-            FirstName: student.FirstName,
-            LastName: student.LastName,
-            PhoneNumber: student.PhoneNumber,
-            StateCode: student.StateCode.StateCode,
-            StateName: student.StateCode.StateName
-        }
+    return result.map((student: { _id: string; FirstName: string; LastName: string; PhoneNumber: Number; StateCode: { StateCode: string; StateName: string } }) => {
+        return studentResponse(student);
     });
 }
 
 export async function createStudent(student: any): Promise<any> {
-const myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     const response = await fetch(`${API_URL}/student/create`, {
         method: 'POST',
